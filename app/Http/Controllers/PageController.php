042c70282762633173bd28a3b2e471165ef072aa
Alexander Gottlieb <alexander@Alexanders-MacBook-Pro-3.local>
2017-01-25 04:04:32 +0000

    Hello World!

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller {
+	
+	public function home() {
+		//TODO set up variables
+		//User logged in?
+			//Name
+			//Location
+			//Basket
+		
+		//Nearby food
+		
+		return view('home');
+	}
+	
+	public function account() {
+		//If not logged in
+			//Login or register
+			//return view('login');
+		//else
+		return view('account');
+	}
+	
+	public function share() {
+		//If not logged in
+			//Login or register
+			//return view('login');
+		//else
+		return view('share');
+	}
+	
+}
